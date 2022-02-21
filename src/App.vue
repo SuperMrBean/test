4f1628a32df173c2bed96d9d0fa983291fe0ddb1
孙志斌 <sunzhibin@corp.netease.com>
2022-02-21 16:38:22 +0800

    feat: test

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,7 @@
 </script>
 
 <template>
-  <img
-    alt="Vue logo"
-    src="./assets/logo.png"
-  >
+  <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
 </template>
 
